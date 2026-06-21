1fe0613893c66a3d25ce8217fa61abf01719dd0a
rsouza-b Ricardo Brenelli <35534617+rbrenelli@users.noreply.github.com>
2026-06-20 22:31:58 -0300

    Minimize Pages deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_PAGES === 'true' ? '/workout-tracker-aistudio/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
